36b69c24b13939c33ce604b59bb67f8efda3783b
Anton <hramickih@yandex.ru>
2023-01-18 15:44:10 +0000

    feat: add symbol

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const MORSE_TABLE = {
+const MORSE_TABLES = {
     '.-': 'a',
     '-...': 'b',
     '-.-.': 'c',
@@ -41,7 +41,7 @@ const MORSE_TABLE = {
 function decode(expr) {
     let words = [];
     for (let i = 0; i < expr.length; i += 10) {
-        words.push(MORSE_TABLE[expr.substring(i, i + 10).replace(/11/g, '-').replace(/10/g, '.').replace(/00/g, '')]);
+        words.push(MORSE_TABLES[expr.substring(i, i + 10).replace(/11/g, '-').replace(/10/g, '.').replace(/00/g, '')]);
     }
     return words.join('');
 }
